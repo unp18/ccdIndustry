5fffb3953a8c7a9b5ccbb239b0bbcc4557479033
unp18 <156875884+unp18@users.noreply.github.com>
2025-09-01 13:06:29 +0530

    Update FileUpload.jsx

--- Frontend/src/Components/FileUpload.jsx
+++ Frontend/src/Components/FileUpload.jsx
@@ -130,7 +130,7 @@ const FileUpload = () => {
           </button>
           <a
             href="/sample.csv"
-            download
+            download = "sample.csv"
             className="px-4 py-2 bg-green-600 hover:bg-green-700 text-white rounded-md shadow-md focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-100 transition-colors flex items-center"
           >
             Download sample CSV
